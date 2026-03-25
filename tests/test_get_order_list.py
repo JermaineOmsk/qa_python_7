07646d2b27ae87285a21137b119278647f944c35
JermaineOmsk <jermaine1989@yandex.ru>
2026-03-25 10:54:29 +0600

    добавил проверку текста ответа

--- tests/test_get_order_list.py
+++ tests/test_get_order_list.py
@@ -5,8 +5,14 @@ from data import *
 
 
 class TestGetOrdersList:
-    @allure.title("Получение списка заказов")
-    def test_get_order_list(self):
+    @allure.title("Получение списка заказов.Проверка кода ответа")
+    def test_get_order_list_code(self):
         response = requests.get(f'{Urls.url}{Endpoints.get_orders_list}')
         assert response.status_code == 200
+        
+
+    @allure.title("Получение списка заказов. Проверка текста ответа")
+    def test_get_order_list_text(self):
+        response = requests.get(f'{Urls.url}{Endpoints.get_orders_list}')
+        assert Response.orders_list in response.json()
         
