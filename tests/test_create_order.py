89561fd375ba9c5391360793c3f62e8c822e2645
JermaineOmsk <jermaine1989@yandex.ru>
2026-03-25 10:53:53 +0600

    использовал параметр json() вместо data

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -2,7 +2,7 @@ import allure
 import requests
 import pytest
 from data import *
-import json
+
 class TestCreateOrder:
     @allure.title("Параметризованный тест.Создание заказа с разными цветами самоката ")
     @pytest.mark.parametrize("color_value", [
@@ -22,7 +22,7 @@ class TestCreateOrder:
                     "deliveryDate": User.deliveryDate,
                     "comment":User.comment,
                     "color": color_value}
-        payload_string = json.dumps(payload)
-        response = requests.post(f'{Urls.url}{Endpoints.create_order}', data=payload_string)
+       
+        response = requests.post(f'{Urls.url}{Endpoints.create_order}', json=payload)
         assert response.status_code == 201  
         assert 'track' in response.json()
